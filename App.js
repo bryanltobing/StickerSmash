1ea88223ab59487df4aa759a81ab10de00c0b395
bryanltobing <bryantobing0@gmail.com>
2023-07-15 18:37:45 +0800

    feat: configure statusbar and splashscreen also app icon

--- App.js
+++ App.js
@@ -135,7 +135,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
 
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </GestureHandlerRootView>
   );
 }
